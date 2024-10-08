41817337c82db077c650cdd1262f1faf1848dde5
nchenevey1 <113471740+nchenevey1@users.noreply.github.com>
2024-10-08 12:10:22 -0700

    Add files via upload
    Added a new Websocket Send node that sends the image dimensions as well. Update to the "dimfix" version of the plugins

--- __init__.py
+++ __init__.py
@@ -3,10 +3,12 @@ from . import nodes
 NODE_CLASS_MAPPINGS = {
     "NC_LoadImageGIMP": nodes.LoadImageGIMP,
     "NC_LoadMaskGIMP": nodes.LoadMaskGIMP,
-    "NC_SendImageWebSocketGIMP": nodes.SendImageWebSocketGIMP
+    "NC_SendImageWebSocketGIMP": nodes.SendImageWebSocketGIMP,
+    "NC_SendImageDimsWebSocketGIMP": nodes.SendImageDimsWebSocketGIMP
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "NC_LoadImageGIMP": "Load Image (GIMP)",
     "NC_LoadMaskGIMP": "Load Mask (GIMP)",
-    "NC_SendImageWebSocketGIMP": "Send Image GIMP (WebSocket)"
+    "NC_SendImageWebSocketGIMP": "Send Image GIMP (WebSocket)",
+    "NC_SendImageDimsWebSocketGIMP": "Send Image with Dimensions GIMP (WebSocket)"
 }
